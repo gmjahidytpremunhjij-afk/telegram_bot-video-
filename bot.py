3d9fc3ace170bfbfb7e28863c64e7fc7db937210
gmjahidytpremunhjij-afk <gmjahidytpremunhjij@gmail.com>
2026-04-13 08:58:51 +0600

    Initialize Telegram bot with start command

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "🤖 Bot working!")
+
+bot.infinity_polling()
